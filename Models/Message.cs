ecef14f6a8500695c4a07a6aff54c75432d44c77
yri066 <yri066.ru@gmail.com>
2024-09-30 12:11:11 +0300

    Добавлена модель сообщения.

---
+++ Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Chat
+{
+    public class Message
+    {
+        [JsonIgnore]
+        public int Id { get; set; }
+        public string SenderId { get; set; } = string.Empty;
+        public string RecipientId { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+    }
+}
