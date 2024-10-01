bb146a135c275c3b3bd839753dddd4b31eb19da1
yri066 <yri066.ru@gmail.com>
2024-10-01 13:40:34 +0300

    Добавлен Api контроллер для сообщений.

---
+++ Controllers/MessageApiController.cs
@@ -0,0 +1,99 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Chat.Controllers
+{
+    [ApiController]
+    [Route("api/[action]")]
+    public class MessageApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IProducer _producer;
+        private readonly ChatConfig _chatConfig;
+
+        public MessageApiController(ApplicationDbContext context, IProducer producer, IOptions<ChatConfig> options)
+        {
+            _context = context;
+            _producer = producer;
+            _chatConfig = options.Value;
+        }
+
+        [HttpGet]
+        public List<Message> GetAllMessages()
+        {
+            return _context.Messages.AsNoTracking().ToList();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetMessagesWithUser(string userId)
+        {
+            if (!_chatConfig.ChatList.Contains(userId))
+            {
+                return NotFound();
+            }
+
+            var currengId = _chatConfig.ClientId.ToString();
+            var list = await _context.Messages.Where(x => x.RecipientId == userId ||
+                                                    x.RecipientId == currengId && x.SenderId == userId)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return Ok(list);
+        }
+
+        [HttpGet]
+        public async Task<List<Message>> GetIncomingMessages()
+        {
+            var currengId = _chatConfig.ClientId.ToString();
+            var list = await _context.Messages.Where(x => x.RecipientId == currengId)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return list;
+        }
+
+        [HttpGet]
+        public async Task<List<Message>> GetSentMessages()
+        {
+            var currengId = _chatConfig.ClientId.ToString();
+            var list = await _context.Messages.Where(x => x.SenderId == currengId)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return list;
+        }
+
+        [HttpPost]
+        public async Task SendMessageToAll(string messageText)
+        {
+            await SendMessage(_chatConfig.PublicChatId, messageText);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> SendMessageToUser(string userId,string messageText)
+        {
+            if (!_chatConfig.ChatList.Contains(userId))
+            {
+                return NotFound();
+            }
+
+            await SendMessage(userId, messageText);
+
+            return Ok();
+        }
+
+        private async Task SendMessage(string userId, string textMessage)
+        {
+            var message = new Message
+            {
+                SenderId = _chatConfig.ClientId.ToString(),
+                RecipientId = userId,
+                Text = textMessage
+            };
+
+            await _producer.SendMessage(message);
+        }
+    }
+}
