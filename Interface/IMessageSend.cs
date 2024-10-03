3d8fd509928fa5c3f0bddc4186b37cd423fead32
yri066 <yri066.ru@gmail.com>
2024-10-03 13:15:30 +0300

    Добавлен класс и интерфейс для отправки сообщений.

---
+++ Interface/IMessageSend.cs
@@ -0,0 +1,13 @@
+﻿namespace Chat.Interface
+{
+    public interface IMessageSend
+    {
+        public Task SendMessage(Message message);
+
+        public Task SendMessageTo(string userId, string textMessage);
+
+        public void SendMessageDelay(Message message, int delay);
+
+        public void SendMessageDelayTo(string userId, string textMessage, int delay);
+    }
+}
