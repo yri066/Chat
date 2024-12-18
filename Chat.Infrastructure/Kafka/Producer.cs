8ddf6ba1f11eca93cbd535083068191142b57f69
yri066 <yri066.ru@gmail.com>
2024-12-18 17:37:34 +0300

    Добавлено взаимодействие с Kafka.

---
+++ Chat.Infrastructure/Kafka/Producer.cs
@@ -0,0 +1,49 @@
+﻿using Chat.Domain;
+using Chat.Domain.Entities;
+using Confluent.Kafka;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace Chat.Infrastructure.Kafka
+{
+    /// <summary>
+    /// Kafka Producer.
+    /// </summary>
+    public class Producer : IProducer
+    {
+        private readonly KafkaConfig _config;
+        private readonly ILogger _logger;
+        public Producer(IOptions<KafkaConfig> options, ILogger<Producer> logger)
+        {
+            _config = options.Value;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Отправить сообщение в Kafka.
+        /// </summary>
+        /// <param name="message">Сообщение.</param>
+        public async Task SendMessage(Message message)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = _config.BootstrapServers,
+            };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                var messageJson = JsonSerializer.Serialize(message);
+
+                try
+                {
+                    await producer.ProduceAsync(_config.Topic, new Message<Null, string> { Value = messageJson });
+                }
+                catch (ProduceException<Null, string> ex)
+                {
+                    _logger.LogError(ex.ToString());
+                }
+            }
+        }
+    }
+}
