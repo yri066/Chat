95ea21d8d0805c9315ac7be60ea5fcd4b93293a5
yri066 <yri066.ru@gmail.com>
2024-12-18 17:28:54 +0300

    Добавлены обработчики консольных действий.

---
+++ Chat.ConsoleWorker/Workers/SendMessageToAll.cs
@@ -0,0 +1,32 @@
+﻿using Chat.Domain.Entities;
+using Chat.Domain.Interface;
+using Chat.Domain.Interface.ConsoleWorker;
+using Chat.Infrastructure.Data;
+using Microsoft.Extensions.Options;
+
+namespace Chat.ConsoleWorker.Workers
+{
+    /// <summary>
+    /// Отправить сообщение всем.
+    /// </summary>
+    public class SendMessageToAll : SendMessageToUser, IWorker
+    {
+        public override string Name => "Отправить сообщение всем пользователям.";
+        private readonly ApplicationDbContext _context;
+        private readonly ChatConfig _chatConfig;
+        private readonly IMessageSend _messageSend;
+
+        public SendMessageToAll(ApplicationDbContext context, IOptions<ChatConfig> options, IMessageSend messageSend) :
+            base(context, options, messageSend)
+        {
+            _context = context;
+            _chatConfig = options.Value;
+            _messageSend = messageSend;
+        }
+
+        protected override string GetUserId(CancellationTokenSource cts)
+        {
+            return _chatConfig.PublicChatId;
+        }
+    }
+}
