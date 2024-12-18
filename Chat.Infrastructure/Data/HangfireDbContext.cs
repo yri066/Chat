5a6ee5ba387dee340124b86ce77cc4427ed6b7a5
yri066 <yri066.ru@gmail.com>
2024-12-18 17:24:04 +0300

    Добавлено взаимодействие с БД.

---
+++ Chat.Infrastructure/Data/HangfireDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Chat.Infrastructure.Data
+{
+    public class HangfireDbContext : DbContext
+    {
+        private string _connectionString;
+        public HangfireDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_connectionString);
+        }
+    }
+}
