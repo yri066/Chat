b547eddb54254b9ce05cd3110d4081a9389294b5
yri066 <yri066.ru@gmail.com>
2024-12-18 17:20:39 +0300

    Добавлены сущности.

---
+++ Chat.Domain/Entities/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace Chat.Domain.Entities
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string SenderId { get; set; } = string.Empty;
+        public string RecipientId { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+    }
+}
